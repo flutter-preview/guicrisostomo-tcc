5cc7e2c623e8c97d125bfbbde4206fe9de825d73
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-18 16:01:41 -0300

    chore: configure route

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:tcc/view/screen_about.dart';
+import 'package:tcc/view/screen_add_product.dart';
+import 'package:tcc/view/screen_home.dart';
+import 'package:tcc/view/screen_info_product.dart';
+import 'package:tcc/view/screen_login.dart';
+import 'package:tcc/view/screen_menu.dart';
+import 'package:tcc/view/screen_presentation.dart';
+import 'package:tcc/view/screen_products.dart';
+import 'package:tcc/view/screen_register.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App pizzaria',
+      initialRoute: 'presentation',
+      routes: {
+        'presentation' :(context) => ScreenPresentation(),
+        'login' :(context) => ScreenLogin(),
+        'register' :(context) => ScreenRegister(),
+        'home' :(context) => ScreenHome(),
+        'menu' :(context) => ScreenMenu(),
+        'about' :(context) => ScreenAbout(),
+        'products' :(context) => ScreenProducts(),
+        'product' :(context) => ScreenInfoProduct(),
+        'add_product' :(context) => ScreenAddProduct(),
+      },
+    )
+  );
+}
