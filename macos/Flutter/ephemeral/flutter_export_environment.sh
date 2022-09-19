95137d5adf8dee69aa63b73911148490ecd0ede3
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-19 19:52:10 -0300

    chore: configurations

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\sdk"
-export "FLUTTER_APPLICATION_PATH=D:\Estudos\Guilherme\Outros\GitHub\tcc\tcc"
+export "FLUTTER_ROOT=D:\Estudos\Guilherme\Outros\GitHub\tcc\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Estudos\Guilherme\Outros\GitHub\tcc"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
