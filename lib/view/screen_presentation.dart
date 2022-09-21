6c1418be3968b46be664cc78f2854ca3e75e70d9
Guilherme Crisostomo <guilherme.silva9872@gmail.com>
2022-09-21 11:45:44 -0300

    feat: adjust elements and created button

--- lib/view/screen_presentation.dart
+++ lib/view/screen_presentation.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tcc/modal/button.dart';
 
 class ScreenPresentation extends StatelessWidget {
   const ScreenPresentation({super.key});
@@ -11,29 +12,46 @@ class ScreenPresentation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(10),
+      body: Padding(
+          padding: EdgeInsets.all(20),
 
           child: Center(
             child: Column(
-              children: [
-                SvgPicture.asset(
-                  assetPresentation,
-                  height: 220,
-                  fit: BoxFit.fill,
-                ),
-
-                Text(
-                  'Peça sua pizza de onde quiser e divida com quem você ama',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+                children: [
+                  SizedBox(height: 0,),
+                  Container(
+                    child: Column(
+                      children: [
+                        SvgPicture.asset(
+                          assetPresentation,
+                          height: 220,
+                          fit: BoxFit.fill,
+                        ),
+                        SizedBox(height: 14,),
+                        Text(
+                          'Peça sua pizza de onde quiser e divida com quem você ama',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-            ]),
+                  
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      button('Entrar', context, 'login'),
+                      button('Cadastrar', context, 'register'),
+                    ],
+                  )
+              ]),
           ),
         ),
-      ),
     );
   }
 }
