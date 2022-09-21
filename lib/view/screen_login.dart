3db4f446308392fc0bf567aaaf0ef90743450ad5
Guilherme Crisostomo <guilherme.silva9872@gmail.com>
2022-09-21 11:56:56 -0300

    feat: add elements

--- lib/view/screen_login.dart
+++ lib/view/screen_login.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tcc/modal/button.dart';
+import 'package:tcc/modal/textField.dart';
 
-class ScreenLogin extends StatelessWidget {
+class ScreenLogin extends StatefulWidget {
   const ScreenLogin({super.key});
+
+  @override
+  State<ScreenLogin> createState() => _ScreenLoginState();
+}
+
+class _ScreenLoginState extends State<ScreenLogin> {
+  var txtEmail = TextEditingController();
+  var txtSenha = TextEditingController();
+
+  final String assetRegister = 'lib/images/imgRegister.svg';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center ,
+          children: [
+            SvgPicture.asset(
+              assetRegister,
+              height: 220,
+              fit: BoxFit.fill,
+            ),
 
+            SizedBox(height: 50,),
+            textField('E-mail', txtEmail),
+            textField('Senha', txtSenha),
+            button('Entrar', context, 'home'),
+            SizedBox(height: 50,),
+
+            Text(
+              'Ainda não se registrou ?',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+
+            button('Registrar agora', context, 'register')
+        ],),
+      ),
     );
   }
 }
