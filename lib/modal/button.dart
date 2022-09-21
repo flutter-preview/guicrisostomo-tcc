9dbb8ceb1198280cbe3773585892090fb8495980
Guilherme Crisostomo <guilherme.silva9872@gmail.com>
2022-09-21 11:43:02 -0300

    feat: add component button

---
+++ lib/modal/button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+button(text, context, screen) {
+    return Container(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(300, 50),
+          primary: Color.fromRGBO(50, 62, 64, 1),
+          
+        ),
+        
+        child: Text(text,
+          style: TextStyle(
+          fontSize: 24,
+        )
+        ),
+
+        //COMPORTAMENTO
+        onPressed: () {
+          Navigator.pushNamed(
+            context,
+            screen,
+          );
+        },
+      ),
+    );
+  }
