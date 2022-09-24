9e5dd37813f15ca713b3dd759ce0a35fc276a083
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-24 14:39:35 -0300

    feat: add screen profile and order

---
+++ lib/view/screen_order.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScreenOrder extends StatefulWidget {
+  const ScreenOrder({super.key});
+
+  @override
+  State<ScreenOrder> createState() => _ScreenOrderState();
+}
+
+class _ScreenOrderState extends State<ScreenOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
