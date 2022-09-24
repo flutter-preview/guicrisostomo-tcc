fefdf27d5962328185c7b848d2699ee2d03d4380
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-24 14:37:42 -0300

    feat: import component bottomNavigation

--- lib/view/screen_home.dart
+++ lib/view/screen_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:tcc/modal/bottonNavigation.dart';
 
 class ScreenHome extends StatefulWidget {
   const ScreenHome({super.key});
