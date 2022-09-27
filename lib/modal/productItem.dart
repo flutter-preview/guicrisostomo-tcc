171dd373ddd480fa998f54aaeac474a10b8261b2
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-27 13:12:19 -0300

    fix elevatedbutton

--- lib/modal/productItem.dart
+++ lib/modal/productItem.dart
@@ -31,54 +31,77 @@ productItem() {
               ),
 
               Text(
-                'PIZZA - GIGANTE',
+                'Pedidos as',
                 style: TextStyle(
                   color: Colors.white,
                 ),
-              )],
+              ),
+              
+              Text(
+                "R\$ 33,00",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              )
+              ],
             )
             
           ),
 
-          trailing: Column(
-            children: [
+          trailing: Container(
+            child: Column(
+              children: [
               
-              
-              SizedBox(height: 0,),
-              ElevatedButton(
-                //constraints: BoxConstraints(maxWidth: 20, maxHeight: 20),
-                onPressed: () {
-                
-                },
-                
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(8, 8),
-                  backgroundColor: Color.fromRGBO(242, 169, 34, 1),
-                  shape: CircleBorder(),
-                  primary: Colors.white,
+              Expanded(
+              child: SizedBox(
+                width: 25,
+                height: 25,
+                child: ElevatedButton(
+                    
+                    onPressed: () {
+                    
+                    },
+                    
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(2),
+                      backgroundColor: Color.fromRGBO(242, 169, 34, 1),
+                      shape: CircleBorder(),
+                      primary: Colors.white,
+                    ),
+                    child: Icon(Icons.add, size: 15, color: Colors.white,),
+                  
+                  ),
                 ),
-                
-                child: Icon(Icons.add, size: 5, color: Colors.white,),
-                
               ),
               
-              ElevatedButton(
-                //constraints: BoxConstraints(maxWidth: 20, maxHeight: 20),
-                onPressed: () {
-                
-                },
-                
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromRGBO(242, 169, 34, 1),
-                  shape: CircleBorder(),
-                  primary: Colors.white,
-                  fixedSize: const Size(8, 8),
-                ),
-                
-                child: Icon(Icons.question_mark, size: 5, color: Colors.white,),
-                
-              ),
-            ]
+              SizedBox(height: 10,),
+
+              Expanded(
+                child: SizedBox(
+                  width: 25,
+                  height: 25,
+                  
+                  child: ElevatedButton(
+                    
+                    
+                    onPressed: () {
+                    
+                    },
+                    
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(2),
+                      backgroundColor: Color.fromRGBO(242, 169, 34, 1),
+                      shape: CircleBorder(),
+                      primary: Colors.white,
+                    ),
+                    
+                    child: Icon(Icons.question_mark, size: 15, color: Colors.white,),
+                  
+                    ),
+                  )
+                )
+              ]
+            )
           ),
           //EVENTO DE CLIQUE
           onTap: () {
