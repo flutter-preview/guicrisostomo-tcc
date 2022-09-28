19df0021f03c506ba03755ccc1f806585e72c679
Guilherme Crisostomo <guilherme.silva9872@gmail.com>
2022-09-28 11:51:11 -0300

    feat: add listview

--- lib/view/screen_order.dart
+++ lib/view/screen_order.dart
@@ -13,9 +13,67 @@ class _ScreenOrderState extends State<ScreenOrder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          child: Column(
+            children: [
+              listViewOrder(),
+            ],
+          )
+        ),
+      ),
 
       bottomNavigationBar: Bottom(),
     );
   }
+}
+
+listViewOrder() {
+  return ListView.builder(
+    itemCount: 10,
+    shrinkWrap:true,
+    scrollDirection: Axis.vertical,
+    itemBuilder: (context, index) {
+    return Card(
+      color: Color.fromRGBO(50, 62, 64, 1),
+      child: ListTile(
+
+        title: Text(
+          'Data: 01/12/2022 às 21:35',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Total: R\$ 52,00',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+
+            Text(
+              'Tipo: entrega',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ]
+        ),
+
+        //EVENTO DE CLIQUE
+        onTap: () {
+          
+        },
+        
+      ),
+    );
+      
+    }
+  );
 }
