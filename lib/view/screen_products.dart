37bcdd7b5252e5f5ddd59f82bab8af2589abf01f
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-27 22:31:01 -0300

    feat: add search

--- lib/view/screen_products.dart
+++ lib/view/screen_products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tcc/modal/textField.dart';
 
 import '../modal/bottonNavigationCustomer.dart';
 
@@ -10,10 +11,30 @@ class ScreenProducts extends StatefulWidget {
 }
 
 class _ScreenProductsState extends State<ScreenProducts> {
+  var txtProd = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Color.fromRGBO(50, 62, 64, 1),
+            boxShadow: [
+              BoxShadow(color: Colors.transparent, spreadRadius: 3),
+            ],
+          ),
 
+          child: Column(
+            children: [
+              textField('Procurar item', txtProd),
+              Icon(Icons.search, size: 50, color: Color.fromRGBO(242, 169, 34, 1),),
+            ],
+          ),
+        ),
+      ),
 
       bottomNavigationBar: Bottom(),
     );
