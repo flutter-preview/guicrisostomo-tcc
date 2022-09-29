709e0ff2ba5e5aafa9513a86152f9a72bbfead6a
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-09-28 22:38:57 -0300

    feat: add element profile

--- lib/view/screen_profile.dart
+++ lib/view/screen_profile.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../modal/bottonNavigationCustomer.dart';
 
@@ -13,7 +16,43 @@ class _ScreenProfileState extends State<ScreenProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      'lib/images/imgProfile.svg',
+                      width: 100,
+                    ),
 
+                    Text(
+                      'Rodrigo',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+
+                    Text(
+                      '(16) 99999-9999',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              
+            ]
+          )
+        )
+      ),
 
       bottomNavigationBar: Bottom(),
     );
