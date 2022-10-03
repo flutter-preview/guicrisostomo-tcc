9047c9df665901092a6b4f127c2b30a3035a42e4
Guilherme Crisostomo <guilherme.silva9872@gmail.com>
2022-10-03 11:09:43 -0300

    feat: add new screen

---
+++ lib/view/screen_edit_datas.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScreenEditDatas extends StatefulWidget {
+  const ScreenEditDatas({super.key});
+
+  @override
+  State<ScreenEditDatas> createState() => _ScreenEditDatasState();
+}
+
+class _ScreenEditDatasState extends State<ScreenEditDatas> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
