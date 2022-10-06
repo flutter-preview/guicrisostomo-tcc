7783a86b51bf2b67703b605db3974a77167ebeb6
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-06 17:55:30 -0300

    refactor: change name class

--- lib/view/screen_register.dart
+++ lib/view/screen_register.dart
@@ -51,7 +51,7 @@ class _ScreenRegisterState extends State<ScreenRegister> {
               SizedBox(height: 10,),
               textFieldPassword('Senha', txtPassword, context),
               SizedBox(height: 10,),
-              TextFieldClass(rotulo: 'Confirmar senha', variavel: txtConfirmPassword, fieldPassword: txtPassword.text),
+              TextFieldConfirmPassword(rotulo: 'Confirmar senha', variavel: txtConfirmPassword, fieldPassword: txtPassword.text),
               // textFieldConfirmPassword('Confirmar senha', txtConfirmPassword, context, txtPassword.value),
               SizedBox(height: 50,),
 
