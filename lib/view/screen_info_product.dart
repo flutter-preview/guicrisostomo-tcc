22be0a73442f087a674b382a28107a1ff853cd15
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-08 18:17:45 -0300

    feat: add appbar

--- lib/view/screen_info_product.dart
+++ lib/view/screen_info_product.dart
@@ -1,12 +1,99 @@
 import 'package:flutter/material.dart';
 
+import '../modal/bottonNavigationCustomer.dart';
+
 class ScreenInfoProduct extends StatelessWidget {
+
+  final String imgPizza = 'lib/images/imgPizza.png';
+
   const ScreenInfoProduct({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(170),
+        child: AppBar(
+          titleSpacing: 0,
+          automaticallyImplyLeading: false,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  imgPizza,
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      
+                      IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: Icon(Icons.arrow_back, color: Colors.white),
+                      ),
+                
+                      const Text(
+                        'Informações',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                        ),
+                      ),
+                
+                    ],
+                  ),
+                ),
+              
+                
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: const [
+                      Text(
+                        'TENTAÇÃO',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                
+                      Text(
+                        'Chocolate preto, chocolate branco e creme de morango',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ]
+                  ),
+                ),
+              ]
+            )
+          ),
+        ),
+      ),
+    
 
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            
+          ]
+        )
+      ),
+
+      bottomNavigationBar: const Bottom(),
     );
   }
 }
