5d17fa5601a13a5b2cee1601898b5290988f52a0
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-11-11 23:17:37 -0300

    style: change color text and background drop down button

--- lib/view/widget/dropDownButton.dart
+++ lib/view/widget/dropDownButton.dart
@@ -14,47 +14,62 @@ class DropDown extends StatefulWidget {
 class _DropDownState extends State<DropDown> {
   String? itemSelecionado;
 
-  @override
-  Widget build(BuildContext context) {
-    return(
-      DropdownButton(
-        focusColor: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        underline: Container(),
-        style: const TextStyle(color: Colors.black),
-        hint: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Text(
-            widget.text,
-            style: const TextStyle(
-              color: Colors.black,
-              fontSize: 16,
-            ),
+  Widget dropDownGeneral() {
+    return DropdownButton(
+      dropdownColor: const Color.fromRGBO(50, 62, 64, 1),
+      iconEnabledColor: const Color.fromRGBO(242, 169, 34, 1),
+      borderRadius: BorderRadius.circular(10),
+      underline: Container(),
+      style: const TextStyle(color: Colors.white),
+      hint: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          widget.text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
           ),
         ),
-        isExpanded: true,
-        value: itemSelecionado,
-        items: widget.itemsDropDownButton.map((String item) {
-          return DropdownMenuItem<String> (
-            value: item,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                item,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                ),
+      ),
+      isExpanded: true,
+      value: itemSelecionado,
+      items: widget.itemsDropDownButton.map((String item) {
+        return DropdownMenuItem<String> (
+          value: item,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              item,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
               ),
             ),
-          );
-        }).toList(),
-        onChanged: (dynamic value) {
-          setState(() {
-            itemSelecionado = value;
-          });
-        },
-      )
+          ),
+        );
+      }).toList(),
+      onChanged: (dynamic value) {
+        setState(() {
+          itemSelecionado = value;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: const Color.fromRGBO(50, 62, 64, 1),
+        boxShadow: const [
+          BoxShadow(color: Colors.transparent, spreadRadius: 3),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: dropDownGeneral(),
+      ),
     );
   }
 }
