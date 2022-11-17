a6eaa9f80a07b2e14caa94ca8d917d30f7fd70e2
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-11-17 18:30:09 -0300

    feat: add event on Pressed into floating button

--- lib/view/widget/floatingButton.dart
+++ lib/view/widget/floatingButton.dart
@@ -4,7 +4,7 @@ Widget floatingButton(context) {
   return FloatingActionButton(
     backgroundColor: const Color.fromRGBO(242, 169, 34, 1),
     onPressed: () {
-      
+      Navigator.pushNamed(context, 'cart');
     },
 
     child: const Icon(
