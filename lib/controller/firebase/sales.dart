9bcb4cfd4c977cee5e1f57aecf4900a6961a8ce7
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-11-19 17:54:37 -0300

    feat: add method for get idSale

--- lib/controller/firebase/sales.dart
+++ lib/controller/firebase/sales.dart
@@ -2,11 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class SalesController {
-  void add(status) {
+  void add() {
     FirebaseFirestore.instance.collection('sales').add(
       {
         'uid': FirebaseAuth.instance.currentUser!.uid,
-        'status': status,
+        'status': 0,
       },
     );
   }
@@ -17,5 +17,26 @@ class SalesController {
         'status': status,
       },
     );
+  }
+
+  Future<String?> idSale() async {
+    var uid = FirebaseAuth.instance.currentUser!.uid;
+    var res;
+    await FirebaseFirestore.instance
+        .collection('sales')
+        .where('uid', isEqualTo: uid)
+        .get()
+        .then(
+      (q) {
+        if (q.docs.isNotEmpty) {
+          return res = q.docs[0].id;
+        } else {
+          add();
+          idSale();
+        }
+      },
+    );
+
+    return res;
   }
 }
