f32d5270704196a2c317aead23b7b89d3d27ae5c
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-12-15 15:44:54 -0300

    feat: add function to register on firebase when sign in with google

--- lib/controller/firebase/authGoogle.dart
+++ lib/controller/firebase/authGoogle.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tcc/view/widget/snackBars.dart';
 
-Future<UserCredential> signInGoogle() async {
+Future<UserCredential> signInGoogle(context) async {
   
   // Trigger the authentication flow
   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
@@ -19,20 +19,32 @@ Future<UserCredential> signInGoogle() async {
     accessToken: googleAuth?.accessToken,
     idToken: googleAuth?.idToken,
   );
-
+  
   // Once signed in, return the UserCredential
   return await FirebaseAuth.instance.signInWithCredential(credential);
 }
 
 void signIn(context) async {
-  await signInGoogle().then((value) {
+  await signInGoogle(context).then((value) {
     success(context, 'Usuário autenticado com sucesso');
+
+    FirebaseFirestore.instance.collection('users')
+        .add(
+          {
+            "uid" : value.user?.uid,
+            "name" : value.user?.displayName,
+            "email" : value.user?.email,
+            "phone" : null,
+            "photo" : value.user?.photoURL,
+          }
+        );
 
     Navigator.of(context).pop();
     Navigator.pushNamed(
       context,
       'home',
     );
+    
     //value.additionalUserInfo?.profile!['email']
   }).catchError((onError) {
     error(context, "Ocorreu um erro ao entrar: $onError");
