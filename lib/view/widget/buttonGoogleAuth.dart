c9b9b926c2e32b935908d30366b4fa640da55e02
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-12-15 15:42:51 -0300

    style: change style of the button google auth

---
+++ lib/view/widget/buttonGoogleAuth.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:tcc/controller/firebase/authGoogle.dart';
+
+class GoogleSignInButton extends StatefulWidget {
+  @override
+  _GoogleSignInButtonState createState() => _GoogleSignInButtonState();
+}
+
+class _GoogleSignInButtonState extends State<GoogleSignInButton> {
+  bool _isSigningIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: _isSigningIn
+        ? const CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          )
+        : OutlinedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(50, 62, 64, 1)),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+            onPressed: () async {
+              
+              setState(() {
+                _isSigningIn = true;
+              });
+
+              signIn(context);
+
+              setState(() {
+                _isSigningIn = false;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget> [
+                  Image(
+                    image: AssetImage("lib/images/google_logo.png"),
+                    height: 35.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      'Entrar com Google',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+    );
+  }
+}
