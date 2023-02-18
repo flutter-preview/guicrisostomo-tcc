80d3161a7da94c493e11cabe3da5246170815554
guicrisostomo <guilherme.silva9872@gmail.com>
2023-02-18 14:29:27 -0300

    refactor: remove import not necessary

--- lib/controller/firebase/authGoogle.dart
+++ lib/controller/firebase/authGoogle.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:tcc/controller/firebase/auth.dart';
 import 'package:tcc/view/widget/snackBars.dart';
 
 Future<UserCredential> signInGoogle(context) async {
