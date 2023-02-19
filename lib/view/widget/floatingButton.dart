af35787e39d24683197eaf74213ad2d354ebe07e
guicrisostomo <guilherme.silva9872@gmail.com>
2023-02-18 22:01:40 -0300

    feat: add new button to floating action button

--- lib/view/widget/floatingButton.dart
+++ lib/view/widget/floatingButton.dart
@@ -4,15 +4,38 @@ import 'package:flutter/material.dart';
 import 'package:tcc/globals.dart' as globals;
 
 Widget floatingButton(context) {
-  return FloatingActionButton(
-    backgroundColor: globals.primary,
-    onPressed: () {
-      Navigator.pushNamed(context, 'cart');
-    },
-
-    child: const Icon(
-      Icons.shopping_cart_outlined,
-      color: Colors.white,
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        FloatingActionButton(
+          backgroundColor: globals.primary,
+          onPressed: () {
+            Navigator.pushNamed(context, 'cart');
+          },
+        
+          child: const Icon(
+            Icons.check,
+            color: Colors.white,
+          ),
+        ),
+    
+        const SizedBox(width: 10),
+    
+        FloatingActionButton(
+          backgroundColor: globals.primary,
+          onPressed: () {
+            Navigator.pushNamed(context, 'cart');
+          },
+    
+          child: const Icon(
+            Icons.shopping_cart_rounded,
+            color: Colors.white,
+          ),
+        ),
+      ],
     ),
   );
 }
