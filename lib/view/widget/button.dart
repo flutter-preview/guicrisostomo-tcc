50b5d28f0c42dcb80290a644307dc1547d045956
guicrisostomo <guilherme.silva9872@gmail.com>
2023-02-21 14:55:13 -0300

    feat: add region icon button

--- lib/view/widget/button.dart
+++ lib/view/widget/button.dart
@@ -1,54 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:tcc/globals.dart' as globals;
 
-Widget button(String text, double width, double height, IconData icon, Function() onPressed) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(40),
-        color: globals.primary,
-        boxShadow: [
-          BoxShadow(color: globals.primaryBlack, spreadRadius: 3),
-        ],
-      ),
+Widget button(
+    String text,
+    double width,
+    double height,
+    IconData icon,
+    Function() onPressed,
 
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          maximumSize: Size(width, height), 
-          backgroundColor: globals.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(40),
-          )
+    [
+      bool isLeftIconButon = true
+    ]
+
+  ) {
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: globals.primary,
+          boxShadow: [
+            BoxShadow(color: globals.primaryBlack, spreadRadius: 3),
+          ],
         ),
-        
-        onPressed: () {
-          onPressed();
-        },
-        
-        child: SizedBox(
-          height: height,
-          width: width,
-          child: Wrap(
-            direction: Axis.horizontal,
-            spacing: 10,
-            alignment: WrapAlignment.center,
-            runAlignment: WrapAlignment.center,
-            
-            children: [
-          
-              Icon(icon, size: 30,),
-          
-              Text(
-                text,
-                style: const TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
+
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            maximumSize: Size(width, height), 
+            backgroundColor: globals.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            )
           ),
-        ),
+          
+          onPressed: () {
+            onPressed();
+          },
+          
+          child: SizedBox(
+            height: height,
+            width: width,
+            child: Wrap(
+              direction: Axis.horizontal,
+              spacing: 10,
+              alignment: WrapAlignment.center,
+              runAlignment: WrapAlignment.center,
+              
+              children: [
+                
+                if (isLeftIconButon)
+                  Icon(icon, size: 30,),
+            
+                Text(
+                  text,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
 
-      ),
-    );
-  }
+                if (!isLeftIconButon)
+                  Icon(icon, size: 30,),
+              ],
+            ),
+          ),
+
+        ),
+      );
+    }
