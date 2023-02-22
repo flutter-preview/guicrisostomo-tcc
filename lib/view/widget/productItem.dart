c2ed944f78adbd4879078f2e0533f7e0d6a9b9e9
guicrisostomo <guilherme.silva9872@gmail.com>
2023-02-22 19:12:30 -0300

    fix: correct error widget

--- lib/view/widget/productItem.dart
+++ lib/view/widget/productItem.dart
@@ -18,157 +18,146 @@ class ProductItem extends StatefulWidget {
 class _ProductItemState extends State<ProductItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: (
-        StreamBuilder<QuerySnapshot>(
-          stream: widget.product.snapshots(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return const Center(
-                  child: Text('Não foi possível conectar.'),
-                );
-              case ConnectionState.waiting:
-                return const Center(child: CircularProgressIndicator());
-              default:
-                final dados = snapshot.requireData;
-                if (dados.size > 0) {
-                  return ListView.builder(
-                    itemCount: dados.size,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      dynamic item = dados.docs[index].data();
-                      String idItem = dados.docs[index].id;
-                      String name = item['name'];
-                      num price = item['price'];
-                      String description = item['description'];
-                      String category = item['category'];
-                      String size = item['size'];
+    return (
+      StreamBuilder<QuerySnapshot>(
+        stream: widget.product.snapshots(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return const Center(
+                child: Text('Não foi possível conectar.'),
+              );
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
+            default:
+              final dados = snapshot.requireData;
+              if (dados.size > 0) {
+                return ListView.builder(
+                  itemCount: dados.size,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    dynamic item = dados.docs[index].data();
+                    String idItem = dados.docs[index].id;
+                    String name = item['name'];
+                    num price = item['price'];
+                    String description = item['description'];
+                    String category = item['category'];
+                    String size = item['size'];
     
-                      return Card(
-                        color: Colors.white,
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
-                          leading: Icon(Icons.local_pizza, size: 50, color: globals.primary),
-                          
-                          title: Text(
-                            name,
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
+                    return Card(
+                      color: Colors.white,
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
+                        leading: Icon(Icons.local_pizza, size: 50, color: globals.primary),
+                        
+                        title: Text(
+                          name,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
                           ),
+                        ),
     
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-
-                              const SizedBox(
-                                height: 3,
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+    
+                            Text(
+                              '$category - $size',
+                              style: TextStyle(
+                                color: Colors.black,
                               ),
-
-                              Text(
-                                '$category - $size',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
+                            ),
+                            
+                            Text(
+                              'R\$ ${price.toStringAsFixed(2).replaceFirst('.', ',')}',
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            )
+                          ]
+                        ),
+    
+                        trailing: Wrap(
+                          direction: Axis.horizontal,
+                          spacing: 20,
+                          children: [
+                          
+                          
+                          SizedBox(
+                            width: 30,
+                            height: 30,
+                            child: ElevatedButton(
+                                
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  'products/add_product',
+                                  arguments: dados.docs[index],
+                                );
+                              },
+                              
+                              style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.all(2),
+                                backgroundColor: globals.primary,
+                                shape: const CircleBorder(),
+                                foregroundColor: Colors.white,
+                              ),
+    
+                              child: const Icon(
+                                Icons.add, size: 20,
+                                color: Colors.white,
                               ),
                               
-                              Text(
-                                'R\$ ${price.toStringAsFixed(2).replaceFirst('.', ',')}',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
-                              )
-                            ]
-                          ),
-    
-                          trailing: Wrap(
-                            direction: Axis.horizontal,
-                            children: [
-                            
-                            
-                            Expanded(
-                              child: SizedBox(
-                                width: 30,
-                                height: 30,
-                                child: ElevatedButton(
-                                    
-                                  onPressed: () {
-                                    Navigator.pushNamed(
-                                      context,
-                                      'products/add_product',
-                                      arguments: dados.docs[index],
-                                    );
-                                  },
-                                  
-                                  style: ElevatedButton.styleFrom(
-                                    padding: const EdgeInsets.all(2),
-                                    backgroundColor: globals.primary,
-                                    shape: const CircleBorder(),
-                                    foregroundColor: Colors.white,
-                                  ),
-    
-                                  child: const Icon(
-                                    Icons.add, size: 20,
-                                    color: Colors.white,
-                                  ),
-                                  
-                                ),
-                              ),
                             ),
-                            
-                            const SizedBox(width: 20,),
-                            
-                            
-                            Expanded(
-                              child: SizedBox(
-                                width: 30,
-                                height: 30,
-                                
-                                child: ElevatedButton(
-                                  
-                                  
-                                  onPressed: () {
-                                    Navigator.pushNamed(
-                                      context,
-                                      'products/info_product',
-                                    );
-                                  },
-                                  
-                                  style: ElevatedButton.styleFrom(
-                                    padding: const EdgeInsets.all(2),
-                                    backgroundColor: globals.primary,
-                                    shape: const CircleBorder(),
-                                    foregroundColor: Colors.white,
-                                  ),
-                                  
-                                  child: const Icon(Icons.question_mark, size: 20, color: Colors.white,),
-                                
-                                  ),
-                                )
-                              )
-                            ]
                           ),
                           
-                          onTap: () {
-                            Navigator.pushNamed(
-                              context,
-                              'products/info_product',
-                            );
-                          },
-                        )
-                      );
-                    },
-                  );
-                } else {
-                  return Text('Nenhum item foi encontrado');
-                }
-            }
-          },
-        )
-      ),
+                          
+                          SizedBox(
+                            width: 30,
+                            height: 30,
+                            
+                            child: ElevatedButton(
+                              
+                              
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  'products/info_product',
+                                );
+                              },
+                              
+                              style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.all(2),
+                                backgroundColor: globals.primary,
+                                shape: const CircleBorder(),
+                                foregroundColor: Colors.white,
+                              ),
+                              
+                              child: const Icon(Icons.question_mark, size: 20, color: Colors.white,),
+                            
+                              ),
+                            )
+                          ]
+                        ),
+                        
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            'products/info_product',
+                          );
+                        },
+                      )
+                    );
+                  },
+                );
+              } else {
+                return Text('Nenhum item foi encontrado');
+              }
+          }
+        },
+      )
     );
   }
 }
