cd3e61ca71e93d90fee0c09e07889f360f933954
guicrisostomo <guilherme.silva9872@gmail.com>
2023-02-22 19:24:58 -0300

    refactor: remove code not necessary

--- lib/view/pages/screen_products.dart
+++ lib/view/pages/screen_products.dart
@@ -6,6 +6,7 @@ import 'package:tcc/view/widget/bottonNavigationCustomer.dart';
 import 'package:tcc/view/widget/floatingButton.dart';
 import 'package:tcc/view/widget/productItem.dart';
 import 'package:tcc/globals.dart' as globals;
+import 'package:tcc/view/widget/textFieldGeneral.dart';
 
 class ScreenProducts extends StatefulWidget {
   const ScreenProducts({super.key});
@@ -27,84 +28,25 @@ class _ScreenProductsState extends State<ScreenProducts> {
   
   @override
   Widget build(BuildContext context) {
-    Widget textFieldSearch() {
-      return Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.white,
-            boxShadow: const [
-              BoxShadow(color: Colors.transparent, spreadRadius: 3),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20),
-            
-            child: Container(
-              margin: const EdgeInsets.only(bottom: 15),
-              padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-              
-              constraints: const BoxConstraints( 
-                minWidth: 70,
-              ),
-      
-              child: Center(
-                child: TextFormField(
-                  controller: txtProd,
-      
-                  style: const TextStyle(
-                    fontSize: 24,
-                    color: Colors.black,
-                  ),
-      
-                  decoration: InputDecoration(
-                    labelText: 'Procurar item',
-                    labelStyle: const TextStyle(
-                      fontSize: 24,
-                      color: Colors.black,
-                    ),
-      
-                    enabledBorder: UnderlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      borderSide:  const BorderSide(color: Colors.transparent ),
-                    ),
-                  ),
-      
-                  onChanged: (value) {
-                    setState(() {
-                      list = ProductsController().listSearch(value);
-                    });
-                  },
-                )
-              )
-            )
-          ),
-        ),
-      );
-    }
 
     return Scaffold(
       body: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(color: globals.primary, spreadRadius: 1),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Row(
-                  children: [
-                    textFieldSearch(),
-                    Icon(Icons.search, size: 50, color: globals.primary,),
-                  ],
-                ),
-              ),
+
+            TextFieldGeneral(
+              label: 'Procurar item',
+              variavel: txtProd,
+              context: context,
+              keyboardType: TextInputType.text,
+              onChanged: (value) {
+                setState(() {
+                  list = ProductsController().listSearch(value);
+                });
+              },
+
+              icoSuffix: Icons.search,
             ),
             
             ProductItem(list),
