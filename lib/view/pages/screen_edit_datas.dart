c47dde2d18bb8b784eceb19bb6fd46bbe4204be9
guicrisostomo <guilherme.silva9872@gmail.com>
2023-02-26 15:19:44 -0300

    feat: add option to show or hide personal datas and comment codes for prevent erros

--- lib/view/pages/screen_edit_datas.dart
+++ lib/view/pages/screen_edit_datas.dart
@@ -4,6 +4,7 @@ import 'package:tcc/utils.dart';
 import 'package:tcc/view/widget/bottonNavigationCustomer.dart';
 import 'package:tcc/view/widget/button.dart';
 import 'package:tcc/view/widget/floatingButton.dart';
+import 'package:tcc/view/widget/sectionVisible.dart';
 import 'package:tcc/view/widget/textFieldGeneral.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:tcc/globals.dart' as globals;
@@ -33,7 +34,7 @@ class _ScreenEditDatasState extends State<ScreenEditDatas> {
 
   @override
   Widget build(BuildContext context) {
-    var user = ModalRoute.of(context)!.settings.arguments as dynamic;
+    // var user = ModalRoute.of(context)!.settings.arguments as dynamic;
     var maskFormatter = MaskTextInputFormatter(
       mask: '(##) #####-####', 
       filter: { "#": RegExp(r'[0-9]') },
@@ -42,12 +43,12 @@ class _ScreenEditDatasState extends State<ScreenEditDatas> {
     );
 
 
-    txtName.text = user.data()['name'];
-    txtEmail.text = user.data()['email'];
+    // txtName.text = user.data()['name'];
+    // txtEmail.text = user.data()['email'];
 
-    if (user.data()['phone'] != null) {
-      txtPhone.text = user.data()['phone'];
-    }
+    // if (user.data()['phone'] != null) {
+    //   txtPhone.text = user.data()['phone'];
+    // }
 
     return Scaffold(
       appBar: AppBar(
@@ -59,74 +60,151 @@ class _ScreenEditDatasState extends State<ScreenEditDatas> {
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
 
-        child: Form(
-          key: formKey,
-          autovalidateMode: autoValidation ? AutovalidateMode.always : AutovalidateMode.disabled,
+        child: Column(
+          children: [
+            SectionVisible(
+              nameSection: 'Dados pessoais',
+              child: 
+                Form(
+                  key: formKey,
+                  autovalidateMode: autoValidation ? AutovalidateMode.always : AutovalidateMode.disabled,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20,),
+                
+                      TextFieldGeneral(
+                        label: 'Nome', 
+                        variavel: txtName,
+                        context: context, 
+                        keyboardType: TextInputType.name,
+                        ico: Icons.person,
+                        validator: (value) {
+                          validatorString(value!);
+                        },
+                      ),
+                
+                      const SizedBox(height: 10,),
+                
+                      TextFieldGeneral(
+                        label: 'E-mail', 
+                        variavel: txtEmail,
+                        context: context, 
+                        keyboardType: TextInputType.emailAddress,
+                        ico: Icons.person,
+                        validator: (value) {
+                          validatorEmail(value!);
+                        },
+                      ),
+                
+                      const SizedBox(height: 10,),
+                
+                      TextFieldGeneral(
+                        label: 'Telefone', 
+                        variavel: txtPhone,
+                        context: context, 
+                        keyboardType: TextInputType.number,
+                        ico: Icons.person,
+                        validator: (value) {
+                          validatorPhone(value!);
+                        },
+                        onChanged: (value) => {
+                          if (value.length <= 14) {
+                            txtPhone.value = maskFormatter.updateMask(mask: "(##) ####-#####")
+                          } else {
+                            txtPhone.value = maskFormatter.updateMask(mask: "(##) #####-####")
+                          }
+                        }
+                      ),
+                
+                      const SizedBox(height: 50,),
+                
+                      button('Salvar', 280, 50, Icons.save, () {
+                
+                        if (formKey.currentState!.validate()) {
+                  
+                          // LoginController().updateUser(user.id, txtName.text, txtEmail.text, txtPhone.text, context);
+                
+                        } else {
+                          setState(() {
+                            autoValidation = true;
+                          });
+                        }
+                
+                      })
+                    ]
+                  ),
+                ),
+            ),
 
-          child: Column(
-            children: [
+            // Form(
+            //   key: formKey,
+            //   autovalidateMode: autoValidation ? AutovalidateMode.always : AutovalidateMode.disabled,
+
+            //   child: Column(
+            //     children: [
+            //       TextFieldGeneral(
+            //         label: 'Nome', 
+            //         variavel: txtName,
+            //         context: context, 
+            //         keyboardType: TextInputType.name,
+            //         ico: Icons.person,
+            //         validator: (value) {
+            //           validatorString(value!);
+            //         },
+            //       ),
+
+            //       const SizedBox(height: 10,),
+
+            //       TextFieldGeneral(
+            //         label: 'E-mail', 
+            //         variavel: txtEmail,
+            //         context: context, 
+            //         keyboardType: TextInputType.emailAddress,
+            //         ico: Icons.person,
+            //         validator: (value) {
+            //           validatorEmail(value!);
+            //         },
+            //       ),
+
+            //       const SizedBox(height: 10,),
+
+            //       TextFieldGeneral(
+            //         label: 'Telefone', 
+            //         variavel: txtPhone,
+            //         context: context, 
+            //         keyboardType: TextInputType.number,
+            //         ico: Icons.person,
+            //         validator: (value) {
+            //           validatorPhone(value!);
+            //         },
+            //         onChanged: (value) => {
+            //           if (value.length <= 14) {
+            //             txtPhone.value = maskFormatter.updateMask(mask: "(##) ####-#####")
+            //           } else {
+            //             txtPhone.value = maskFormatter.updateMask(mask: "(##) #####-####")
+            //           }
+            //         }
+            //       ),
+
+            //       const SizedBox(height: 50,),
+
+            //       button('Salvar', 280, 50, Icons.save, () {
+
+            //         if (formKey.currentState!.validate()) {
               
-              TextFieldGeneral(
-                label: 'Nome', 
-                variavel: txtName,
-                context: context, 
-                keyboardType: TextInputType.name,
-                ico: Icons.person,
-                validator: (value) {
-                  validatorString(value!);
-                },
-              ),
+            //           // LoginController().updateUser(user.id, txtName.text, txtEmail.text, txtPhone.text, context);
 
-              const SizedBox(height: 10,),
+            //         } else {
+            //           setState(() {
+            //             autoValidation = true;
+            //           });
+            //         }
 
-              TextFieldGeneral(
-                label: 'E-mail', 
-                variavel: txtEmail,
-                context: context, 
-                keyboardType: TextInputType.emailAddress,
-                ico: Icons.person,
-                validator: (value) {
-                  validatorEmail(value!);
-                },
-              ),
-
-              const SizedBox(height: 10,),
-
-              TextFieldGeneral(
-                label: 'Telefone', 
-                variavel: txtPhone,
-                context: context, 
-                keyboardType: TextInputType.number,
-                ico: Icons.person,
-                validator: (value) {
-                  validatorPhone(value!);
-                },
-                onChanged: (value) => {
-                  if (value.length <= 14) {
-                    txtPhone.value = maskFormatter.updateMask(mask: "(##) ####-#####")
-                  } else {
-                    txtPhone.value = maskFormatter.updateMask(mask: "(##) #####-####")
-                  }
-                }
-              ),
-
-              const SizedBox(height: 50,),
-
-              button('Salvar', 280, 50, Icons.save, () {
-
-                if (formKey.currentState!.validate()) {
-          
-                  LoginController().updateUser(user.id, txtName.text, txtEmail.text, txtPhone.text, context);
-
-                } else {
-                  setState(() {
-                    autoValidation = true;
-                  });
-                }
-
-              })
-            ]
-          ),
+            //       })
+            //     ]
+            //   ),
+            // ),
+          ],
         ),
       ),
 
