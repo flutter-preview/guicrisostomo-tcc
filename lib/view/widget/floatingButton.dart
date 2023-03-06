6096ea1ec5a9e659b534bff005326a61a6d55c51
guicrisostomo <guilherme.silva9872@gmail.com>
2023-03-05 22:53:56 -0300

    style: add gradient to floating buttons

--- lib/view/widget/floatingButton.dart
+++ lib/view/widget/floatingButton.dart
@@ -11,29 +11,55 @@ Widget floatingButton(context) {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       mainAxisSize: MainAxisSize.max,
       children: [
-        FloatingActionButton(
-          backgroundColor: globals.primary,
-          onPressed: () {
-            Navigator.pushNamed(context, 'finalize_order_customer');
-          },
-        
-          child: const Icon(
-            Icons.check,
-            color: Colors.white,
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(2000),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                globals.primary,
+                globals.primary.withOpacity(0.8),
+              ]
+            )
+          ),
+          child: FloatingActionButton(
+            backgroundColor: Colors.transparent,
+            onPressed: () {
+              Navigator.pushNamed(context, 'finalize_order_customer');
+            },
+          
+            child: const Icon(
+              Icons.check,
+              color: Colors.white,
+            ),
           ),
         ),
     
         const SizedBox(width: 10),
     
-        FloatingActionButton(
-          backgroundColor: globals.primary,
-          onPressed: () {
-            Navigator.pushNamed(context, 'cart');
-          },
-    
-          child: const Icon(
-            Icons.shopping_cart_rounded,
-            color: Colors.white,
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(2000),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                globals.primary,
+                globals.primary.withOpacity(0.8),
+              ]
+            )
+          ),
+          child: FloatingActionButton(
+            backgroundColor: Colors.transparent,
+            onPressed: () {
+              Navigator.pushNamed(context, 'cart');
+            },
+            
+            child: const Icon(
+              Icons.shopping_cart_rounded,
+              color: Colors.white,
+            ),
           ),
         ),
       ],
