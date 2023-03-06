a1d58a9f360564aa8b8257337347459b37644f46
guicrisostomo <guilherme.silva9872@gmail.com>
2023-03-05 21:11:35 -0300

    style: add gradient button

--- lib/view/widget/button.dart
+++ lib/view/widget/button.dart
@@ -15,45 +15,58 @@ Widget button(
     ]
 
   ) {
-      return ElevatedButton(
-        
-        style: ElevatedButton.styleFrom(
-          minimumSize: Size(width, height), 
-          backgroundColor: color ?? globals.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          padding: const EdgeInsets.all(10)
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              color ?? globals.primary,
+              color == null ? globals.primary.withOpacity(0.8) : color.withOpacity(0.8),
+            ]
+          )
         ),
-        
-        onPressed: () {
-          onPressed();
-        },
-        
-        child: Wrap(
-          direction: Axis.horizontal,
-          spacing: 10,
-          alignment: WrapAlignment.center,
-          runAlignment: WrapAlignment.center,
+        child: ElevatedButton(
           
-          children: [
-
-            if (isLeftIconButon && icon != null)
-              Icon(icon, size: 30,),
-        
-            Text(
-              text,
-              style: TextStyle(
-                fontSize: fontSize,
-                color: Colors.white,
-                fontWeight: FontWeight.w600,
-              ),
+          style: ElevatedButton.styleFrom(
+            minimumSize: Size(width, height),
+            backgroundColor: Colors.transparent,
+            padding: const EdgeInsets.all(10),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-
-            if (!isLeftIconButon && icon != null)
-              Icon(icon, size: 30,),
-          ],
+          ),
+          
+          onPressed: () {
+            onPressed();
+          },
+          
+          child: Wrap(
+            direction: Axis.horizontal,
+            spacing: 10,
+            alignment: WrapAlignment.center,
+            runAlignment: WrapAlignment.center,
+            
+            children: [
+      
+              if (isLeftIconButon && icon != null)
+                Icon(icon, size: 30,),
+          
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: fontSize,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+      
+              if (!isLeftIconButon && icon != null)
+                Icon(icon, size: 30,),
+            ],
+          ),
+      
         ),
-
       );
     }
