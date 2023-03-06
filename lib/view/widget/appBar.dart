6d025035fc8843411e5f3a31fe1bc01e2ed5b1c4
guicrisostomo <guilherme.silva9872@gmail.com>
2023-03-06 12:24:22 -0300

    refactor: change icon cart local

--- lib/view/widget/appBar.dart
+++ lib/view/widget/appBar.dart
@@ -4,6 +4,7 @@ import 'package:tcc/globals.dart' as globals;
 
 PreferredSizeWidget appBarWidget({
   required String pageName,
+  required BuildContext context,
   IconData? icon,
   String svg = '',
 }) {
@@ -12,6 +13,20 @@ PreferredSizeWidget appBarWidget({
       svg,
       fit: BoxFit.scaleDown,
     ) : Icon(icon, color: Colors.white, size: 30),
+    actions: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+        child: IconButton(
+          icon: Icon(
+            Icons.abc,
+            size: 30,
+          ),
+          onPressed: () { 
+            Navigator.pushNamed(context, '/cart');
+          },
+        ),
+      ),
+    ],
     title: Text(pageName),
     
     flexibleSpace: Container(
--- lib/view/widget/floatingButton.dart
+++ lib/view/widget/floatingButton.dart
@@ -7,62 +7,29 @@ Widget floatingButton(context) {
   print(globals.isUserTyping);
   return !globals.isUserTyping ? Padding(
     padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(2000),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                globals.primary,
-                globals.primary.withOpacity(0.8),
-              ]
-            )
-          ),
-          child: FloatingActionButton(
-            backgroundColor: Colors.transparent,
-            onPressed: () {
-              Navigator.pushNamed(context, 'finalize_order_customer');
-            },
-          
-            child: const Icon(
-              Icons.check,
-              color: Colors.white,
-            ),
-          ),
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(2000),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            globals.primary,
+            globals.primary.withOpacity(0.8),
+          ]
+        )
+      ),
+      child: FloatingActionButton(
+        backgroundColor: Colors.transparent,
+        onPressed: () {
+          Navigator.pushNamed(context, 'finalize_order_customer');
+        },
+      
+        child: const Icon(
+          Icons.check,
+          color: Colors.white,
         ),
-    
-        const SizedBox(width: 10),
-    
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(2000),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                globals.primary,
-                globals.primary.withOpacity(0.8),
-              ]
-            )
-          ),
-          child: FloatingActionButton(
-            backgroundColor: Colors.transparent,
-            onPressed: () {
-              Navigator.pushNamed(context, 'cart');
-            },
-            
-            child: const Icon(
-              Icons.shopping_cart_rounded,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ],
+      ),
     ),
   ) : const SizedBox();
 }
