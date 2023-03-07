bc72cbc363d83e679e1f1bffacf747f9c88ef740
guicrisostomo <guilherme.silva9872@gmail.com>
2023-03-07 11:01:59 -0300

    feat: add widget cart info

--- lib/view/widget/bottonNavigationCustomer.dart
+++ lib/view/widget/bottonNavigationCustomer.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:tcc/view/widget/cartInfo.dart';
 import '../../globals.dart' as globals;
 
 class Bottom extends StatefulWidget {
@@ -18,100 +19,106 @@ class _BottomState extends State<Bottom> {
     const String iconOrder = 'lib/images/iconOrder.svg';
     const String iconMenu = 'lib/images/iconMenu.svg';
 
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            globals.primaryBlack,
-            globals.primary.withOpacity(0.9),
-          ]
-        ),
-      ),
-      child: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: Colors.white,
-        selectedItemColor: Colors.white,
-        backgroundColor: Colors.transparent,
-        selectedLabelStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 14,
-        ),
-    
-    
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.white),
-            label: 'Inicio',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(iconOrder, height: 25, fit: BoxFit.fill,),
-            label: 'Pedidos',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(iconMenu, height: 25, fit: BoxFit.fill,),
-            label: 'Cardápio',
-          ),
-    
-          globals.isSaleInTable ?
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.room_service_outlined, color: Colors.white),
-              label: 'Garçom',
-            ) :
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.table_restaurant, color: Colors.white),
-              label: 'Mesa',
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        cartInfo(context),
+        
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                globals.primaryBlack,
+                globals.primary.withOpacity(0.9),
+              ]
             ),
-    
-          
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.perm_identity, color: Colors.white),
-            label: 'Perfil',
           ),
-        ],
-    
-        currentIndex: globals.globalSelectedIndexBotton,
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, 'home');
-              break;
-            case 1:
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, 'order');
-              break;
-            case 2:
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, 'products');
-              break;
-            case 3:
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.white,
+            backgroundColor: Colors.transparent,
+            selectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            ),
+        
+            items: <BottomNavigationBarItem>[
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: Colors.white),
+                label: 'Inicio',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(iconOrder, height: 25, fit: BoxFit.fill,),
+                label: 'Pedidos',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(iconMenu, height: 25, fit: BoxFit.fill,),
+                label: 'Cardápio',
+              ),
+        
+              globals.isSaleInTable ?
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.room_service_outlined, color: Colors.white),
+                  label: 'Garçom',
+                ) :
+                const BottomNavigationBarItem(
+                  icon: Icon(Icons.table_restaurant, color: Colors.white),
+                  label: 'Mesa',
+                ),
+        
               
-              if (globals.isSaleInTable) {
-                Navigator.of(context).pop();
-                Navigator.pushNamed(context, 'waiter');
-                break;
-              } else {
-                Navigator.of(context).pop();
-                Navigator.pushNamed(context, 'table');
-                break;
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.perm_identity, color: Colors.white),
+                label: 'Perfil',
+              ),
+            ],
+        
+            currentIndex: globals.globalSelectedIndexBotton,
+            onTap: (int index) {
+              switch (index) {
+                case 0:
+                  Navigator.of(context).pop();
+                  Navigator.pushNamed(context, 'home');
+                  break;
+                case 1:
+                  Navigator.of(context).pop();
+                  Navigator.pushNamed(context, 'order');
+                  break;
+                case 2:
+                  Navigator.of(context).pop();
+                  Navigator.pushNamed(context, 'products');
+                  break;
+                case 3:
+                  
+                  if (globals.isSaleInTable) {
+                    Navigator.of(context).pop();
+                    Navigator.pushNamed(context, 'waiter');
+                    break;
+                  } else {
+                    Navigator.of(context).pop();
+                    Navigator.pushNamed(context, 'table');
+                    break;
+                  }
+        
+                case 4:
+                  Navigator.of(context).pop();
+                  Navigator.pushNamed(context, 'profile');
+                  break;
               }
-    
-            case 4:
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, 'profile');
-              break;
-          }
-    
-          setState(
-            () {
-              globals.globalSelectedIndexBotton = index;
+        
+              setState(
+                () {
+                  globals.globalSelectedIndexBotton = index;
+                },
+              );
+              
             },
-          );
-          
-        },
-      ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/view/widget/cartInfo.dart
@@ -0,0 +1,148 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:tcc/globals.dart' as globals;
+
+Widget cartInfo(context) {
+  return !globals.isUserTyping ? Container(
+    margin: const EdgeInsets.fromLTRB(0, 0, 10, 10),
+    child: Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, 'cart');
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  globals.primaryBlack,
+                  globals.primary.withOpacity(0.9),
+                ]
+              ),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+        
+                    SizedBox(width: 5),
+        
+                    Text(
+                      '1',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+    
+                const SizedBox(width: 5),
+        
+                const Text(
+                  'Item(s)',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+    
+                const SizedBox(width: 5),
+        
+                Row(
+                  children: const [
+                    Text(
+                      'R\$ 10,00',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+        
+                    SizedBox(width: 5),
+        
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+    
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, 'finalize_order_customer');
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  globals.primaryBlack,
+                  globals.primary.withOpacity(0.9),
+                ]
+              ),
+            ),
+        
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: const [
+                Icon(
+                  Icons.check,
+                  color: Colors.white,
+                  size: 20
+                ),
+    
+                SizedBox(width: 5),
+          
+                Text(
+                  'Finalizar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+    
+                SizedBox(width: 5),
+          
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                  size: 15
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ),
+  ) : const SizedBox();
+}
