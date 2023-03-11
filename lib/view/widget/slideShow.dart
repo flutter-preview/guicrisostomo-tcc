6eea54ae60aeb650bc5bf8c27e3dda805daedd60
guicrisostomo <guilherme.silva9872@gmail.com>
2023-03-11 12:45:41 -0300

    feat: improve position of text and add icon and circle

--- lib/view/widget/slideShow.dart
+++ lib/view/widget/slideShow.dart
@@ -14,47 +14,90 @@ class SlideShowWidget extends StatefulWidget {
 }
 
 class _SlideShowWidgetState extends State<SlideShowWidget> {
+  PageController indicator = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: 200,
-      child: PageView(
-        children: [
-          for (int i = 0; i < widget.listSlideShow.length; i++)
-            ElevatedButton(
-              onPressed: widget.listSlideShow[i].onTap,
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.transparent),
-                shadowColor: MaterialStateProperty.all(Colors.transparent),
-                padding: MaterialStateProperty.all(EdgeInsets.zero),
-              ),
-              child: Container(
-                
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 200,
+          child: PageView(
+            controller: indicator,
+            children: [
+              for (int i = 0; i < widget.listSlideShow.length; i++)
+                ElevatedButton(
+                  onPressed: widget.listSlideShow[i].onTap,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                    shadowColor: MaterialStateProperty.all(Colors.transparent),
+                    padding: MaterialStateProperty.all(EdgeInsets.zero),
+                  ),
+                  child: Container(
+                    
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          widget.listSlideShow[i].path,
+                        ),
+                        opacity: 0.5,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Row(
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              widget.listSlideShow[i].title,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+
+                          const Spacer(),
+
+                          const Align(
+                            alignment: Alignment.bottomRight,
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int i = 0; i < widget.listSlideShow.length; i++)
+              Container(
+                width: 10,
+                height: 10,
+                margin: const EdgeInsets.symmetric(horizontal: 2),
                 decoration: BoxDecoration(
-                  color: Colors.black,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      widget.listSlideShow[i].path,
-                    ),
-                    opacity: 0.5,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    widget.listSlideShow[i].title,
-                    style: const TextStyle(
-                      color: Colors.white60,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
+                  shape: BoxShape.circle,
+                  color: i == indicator.page!.round()
+                      ? Colors.black
+                      : Colors.grey,
                 ),
               ),
-            )
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
