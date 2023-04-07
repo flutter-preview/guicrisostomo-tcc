98dd0924f6831d353dfa4e0ffa58b52a6df9175c
guicrisostomo <guilherme.silva9872@gmail.com>
2023-04-07 14:50:52 -0300

    config: add variable global (business)

--- lib/globals.dart
+++ lib/globals.dart
@@ -10,3 +10,4 @@ Color primary = const Color.fromRGBO(186, 49, 39, 1);
 Color secundary = const Color.fromRGBO(234, 182, 47, 1);
 Color primaryBlack = const Color.fromARGB(255, 146, 38, 30);
 String userType = '';
+String businessId = '0';
