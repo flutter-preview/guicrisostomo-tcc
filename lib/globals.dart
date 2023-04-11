f9ea3e9f81d9d7d10e3f60033b582b4b5837d100
guicrisostomo <guilherme.silva9872@gmail.com>
2023-04-11 17:53:52 -0300

    remove variable

--- lib/globals.dart
+++ lib/globals.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 int globalSelectedIndexBotton = 0;
 bool isSaleInTable = false;
 bool isUserTyping = false;
-bool isSaleNull = true;
 Color primary = const Color.fromRGBO(186, 49, 39, 1);
 Color secundary = const Color.fromRGBO(234, 182, 47, 1);
 Color primaryBlack = const Color.fromARGB(255, 146, 38, 30);
