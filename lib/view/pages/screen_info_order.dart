57a574af28f2344d4dd0f30c99f2c56a1ec9c643
guicrisostomo <guilherme.silva9872@gmail.com>
2023-05-19 15:05:01 -0300

    fix: correct datas info order

--- lib/view/pages/screen_info_order.dart
+++ lib/view/pages/screen_info_order.dart
@@ -1,9 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:tcc/controller/postgres/Lists/productsCart.dart';
+import 'package:tcc/model/ProductsCart.dart';
+import 'package:tcc/model/Sales.dart';
+import 'package:tcc/view/widget/appBar.dart';
 import 'package:tcc/view/widget/bottonNavigation.dart';
 import 'package:tcc/globals.dart' as globals;
+import 'package:tcc/view/widget/listCart.dart';
+import 'package:tcc/view/widget/sectionVisible.dart';
 
 class ScreenInfoOrder extends StatefulWidget {
   Object? arguments;
@@ -18,168 +22,55 @@ class ScreenInfoOrder extends StatefulWidget {
 }
 
 class _ScreenInfoOrderState extends State<ScreenInfoOrder> {
+  List<ProductsCartList> list = [];
+
+  Sales orderSelect = Sales(id: 0, uid: '0', cnpj: '0', status: '', date: DateTime.now());
+
+  num total = 0;
+  int id = 0;
+  DateTime date = DateTime.now();
+  String textDate = '';
+  
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      orderSelect = widget.arguments as Sales;
+
+      date = orderSelect.date;
+      total = orderSelect.total;
+      id = orderSelect.id;
+      textDate = DateFormat("d 'de' MMMM 'de' y 'às' HH':'mm':'ss", "pt_BR").format(date);
+    });
+    
+    
+    getList().then((value) {
+      setState(() {
+        list = value;
+      });
+    });
+  }
+  
+  Future<List<ProductsCartList>> getList() async {
+    return await ProductsCartController().list(orderSelect.id).then((value) {
+      return value;
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var orderSelect = widget.arguments as QueryDocumentSnapshot;
-
-    dynamic item = orderSelect.data();
-    Map<String, dynamic> map = item!;
-    DateTime date = (map['date'] as Timestamp).toDate();
-    num total = orderSelect['total'];
-    String? id = orderSelect.id.toString();
-    String textDate = DateFormat("d 'de' MMMM 'de' y 'às' HH':'mm':'ss", "pt_BR").format(date);
-    
-    // Widget productItem() {
-    //   return Container(
-    //     child: (
-    //       StreamBuilder<QuerySnapshot>(
-    //         stream: ProductsCartController().list(id).snapshots(),
-    //         builder: (context, snapshot) {
-    //           switch (snapshot.connectionState) {
-    //             case ConnectionState.none:
-    //               return const Center(
-    //                 child: Text('Não foi possível conectar.'),
-    //               );
-    //             case ConnectionState.waiting:
-    //               return const Center(child: CircularProgressIndicator());
-    //             default:
-    //               final dados = snapshot.requireData;
-    //               if (dados.size > 0) {
-    //                 return ListView.builder(
-    //                   itemCount: dados.size,
-    //                   shrinkWrap: true,
-    //                   itemBuilder: (context, index) {
-    //                     dynamic item = dados.docs[index].data();
-    //                     String name = item['name'];
-    //                     num price = item['price'];
-    //                     String category = item['category'];
-    //                     String size = item['size'];
-
-    //                     return Card(
-    //                       color: globals.primary,
-    //                       child: ListTile(
-    //                         contentPadding: const EdgeInsets.fromLTRB(10, 10, 20, 10),
-    //                         leading: const Icon(Icons.local_pizza, size: 50, color: Colors.white),
-                            
-    //                         title: Text(
-    //                           name,
-    //                           style: const TextStyle(
-    //                             fontSize: 24,
-    //                             color: Colors.white,
-    //                           ),
-    //                         ),
-      
-    //                         subtitle: Column(
-    //                           crossAxisAlignment: CrossAxisAlignment.start,
-    //                           children: [
-    //                             Text(
-    //                               '$category - $size',
-    //                               style: const TextStyle(
-    //                                 color: Colors.white,
-    //                               ),
-    //                             ),
-                                
-    //                             Text(
-    //                               'R\$ ${price.toStringAsFixed(2).replaceFirst('.', ',')}',
-    //                               style: const TextStyle(
-    //                                 color: Colors.white,
-    //                               ),
-    //                             )
-    //                           ]
-    //                         ),
-      
-    //                         /*trailing: Column(
-    //                           children: [
-                              
-                              
-    //                           Expanded(
-    //                             child: SizedBox(
-    //                               width: 25,
-    //                               height: 25,
-    //                               child: ElevatedButton(
-                                      
-    //                                 onPressed: () {
-    //                                   Navigator.pushNamed(
-    //                                     context,
-    //                                     'products/add_product',
-    //                                     arguments: dados.docs[index],
-    //                                   );
-    //                                 },
-                                    
-    //                                 style: ElevatedButton.styleFrom(
-    //                                   padding: const EdgeInsets.all(2),
-    //                                   backgroundColor: secundary,
-    //                                   shape: const CircleBorder(),
-    //                                   foregroundColor: Colors.white,
-    //                                 ),
-      
-    //                                 child: const Icon(
-    //                                   Icons.add, size: 15,
-    //                                   color: Colors.white,
-    //                                 ),
-                                    
-    //                               ),
-    //                             ),
-    //                           ),
-                              
-    //                           const SizedBox(height: 10,),
-                              
-                              
-    //                           Expanded(
-    //                             child: SizedBox(
-    //                               width: 25,
-    //                               height: 25,
-                                  
-    //                               child: ElevatedButton(
-                                    
-                                    
-    //                                 onPressed: () {
-    //                                   Navigator.pushNamed(
-    //                                     context,
-    //                                     'products/info_product',
-    //                                   );
-    //                                 },
-                                    
-    //                                 style: ElevatedButton.styleFrom(
-    //                                   padding: const EdgeInsets.all(2),
-    //                                   backgroundColor: secundary,
-    //                                   shape: const CircleBorder(),
-    //                                   foregroundColor: Colors.white,
-    //                                 ),
-                                    
-    //                                 child: const Icon(Icons.question_mark, size: 15, color: Colors.white,),
-                                  
-    //                                 ),
-    //                               )
-    //                             )
-    //                           ]
-    //                         ),
-                            
-    //                         onTap: () {
-    //                           Navigator.pushNamed(
-    //                             context,
-    //                             'products/info_product',
-    //                           );
-    //                         },*/
-    //                       )
-    //                     );
-    //                   },
-    //                 );
-    //               } else {
-    //                 return const Text('Nenhum item foi encontrado');
-    //               }
-    //           }
-    //         },
-    //       )
-    //     ),
-    //   );
-    // }
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Informações'),
-        centerTitle: true,
-        backgroundColor: globals.primary,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBarWidget(
+          context: context,
+          pageName: 'Pedido $id',
+          withoutIcons: true,
+        ),
       ),
 
       body: SingleChildScrollView(
@@ -188,6 +79,59 @@ class _ScreenInfoOrderState extends State<ScreenInfoOrder> {
           
           child: Column(
             children: [
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline, size: 30, color: globals.primary),
+                  const SizedBox(width: 10,),
+                  Text(
+                    'Status: ${orderSelect.status}',
+                  )
+                ],
+              ),
+
+              const SizedBox(height: 10,),
+
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.delivery_dining, size: 30, color: globals.primary),
+                  const SizedBox(width: 10,),
+                  Text(
+                    'Tipo da venda: ${orderSelect.type}',
+                  )
+                ],
+              ),
+
+              const SizedBox(height: 10,),
+
+              Row(
+                children: [
+                  Icon(Icons.credit_card_sharp, size: 30, color: globals.primary),
+                  const SizedBox(width: 10,),
+                  Text(
+                    'Forma de pagamento: ${orderSelect.payment}',
+                  )
+                ],
+              ),
+
+              if (orderSelect.type == 'Mesa')
+                Column(
+                  children: [
+                    const SizedBox(height: 10,),
+
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.people_rounded, size: 30, color: globals.primary),
+                        const SizedBox(width: 10,),
+                        Text(
+                          'Mesa: ${orderSelect.table}',
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+
               /*Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: const [
@@ -199,12 +143,13 @@ class _ScreenInfoOrderState extends State<ScreenInfoOrder> {
                 ],
               ),
       
-              const SizedBox(height: 5,),*/
-      
+              const SizedBox(height: 10,),*/
+              const SizedBox(height: 10,),
+
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Icon(Icons.timer_outlined, size: 30, color: Color.fromRGBO(242, 169, 34, 1)),
+                  Icon(Icons.timer_outlined, size: 30, color: globals.primary),
                   const SizedBox(width: 10,),
                   Text(
                     'Data: $textDate',
@@ -212,7 +157,7 @@ class _ScreenInfoOrderState extends State<ScreenInfoOrder> {
                 ],
               ),
       
-              const SizedBox(height: 5,),
+              const SizedBox(height: 10,),
       
               /*Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -225,12 +170,12 @@ class _ScreenInfoOrderState extends State<ScreenInfoOrder> {
                 ],
               ),
       
-              const SizedBox(height: 5,),*/
+              const SizedBox(height: 10,),*/
       
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Icon(Icons.attach_money, size: 30, color: Color.fromRGBO(242, 169, 34, 1)),
+                  Icon(Icons.attach_money, size: 30, color: globals.primary),
                   const SizedBox(width: 10,),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -238,29 +183,38 @@ class _ScreenInfoOrderState extends State<ScreenInfoOrder> {
                       Text(
                         'Total: R\$ ${total.toStringAsFixed(2).replaceFirst('.', ',')}',
                       ),
-      
-                      /*Text(
-                        'Pago em dinheiro',
-                      )*/
+
+                      const SizedBox(height: 5,),
+
+                      if (orderSelect.payment != null)
+                        Text(
+                          'Pago em ${orderSelect.payment}',
+                        )
                     ]
                   )
                 ],
               ),
       
-              const SizedBox(height: 10,),
+              const SizedBox(height: 20,),
       
-              Container(
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  'Itens pedidos',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                  ),
-                )
-              ),
+              SectionVisible(
+                nameSection: 'Itens do pedido',
+                isShowPart: true,
+                child: FutureBuilder(
+                  future: getList(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return ProductsCart(
+                        product: list,
+                      );
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  }
+                ),
+              )
       
               // productItem(),
             ]
