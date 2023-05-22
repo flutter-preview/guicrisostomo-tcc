dad55615eed3e4faa7d85717a7bd19cc311e16a8
guicrisostomo <guilherme.silva9872@gmail.com>
2023-05-22 15:47:39 -0300

    add image and text to screen

--- lib/view/pages/screen_loading.dart
+++ lib/view/pages/screen_loading.dart
@@ -13,15 +13,35 @@ class _ScreenLoadingState extends State<ScreenLoading> {
   Future<bool> _onWillPop() async {
     return false; //<-- SEE HERE
   }
+
+  String loadingPath = 'lib/images/loading.gif';
   
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: _onWillPop,
-      child: const Scaffold(
+      child: Scaffold(
         body: Center(
-          child: CircularProgressIndicator.adaptive(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            
+            children: [
+              Image.asset(
+                loadingPath,
+                fit: BoxFit.cover,
+              ),
+        
+              const SizedBox(height: 20,),
+        
+              const Text(
+                'Carregando...',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red
+                ),
+              ),
+            ],
           ),
         ),
       ),
