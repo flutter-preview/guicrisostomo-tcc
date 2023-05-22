4c6df4f4c930e75ab0eb486ab383ff4a66df6fbe
guicrisostomo <guilherme.silva9872@gmail.com>
2023-05-21 21:43:41 -0300

    improve method verify email

--- lib/view/pages/screen_verify_email.dart
+++ lib/view/pages/screen_verify_email.dart
@@ -14,19 +14,23 @@ class ScreenVerifyEmail extends StatefulWidget {
 
 class _ScreenVerifyEmailState extends State<ScreenVerifyEmail> {
   
-  @override
-  Widget build(BuildContext context) {
-    Future<bool> isEmailVerified() async {
-      User? user = FirebaseAuth.instance.currentUser;
-      return user!.emailVerified;
+  bool isEmailVerified() {
+    FirebaseAuth.instance.currentUser!.reload();
+
+    if (FirebaseAuth.instance.currentUser?.emailVerified == true) {
+      LoginController().redirectUser(context);
+      return true;
     }
 
-    isEmailVerified().then((value) async {
-      if (value) {
-        await LoginController().redirectUser(context);
-        success(context, 'E-mail verificado com sucesso!');
-      }
-    });
+    if (FirebaseAuth.instance.currentUser?.emailVerified == null) {
+      return false;
+    } else {
+      return FirebaseAuth.instance.currentUser!.emailVerified;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: PreferredSize(
@@ -38,49 +42,92 @@ class _ScreenVerifyEmailState extends State<ScreenVerifyEmail> {
         ),
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const SizedBox(height: 20,),
-
-            const Text(
-              'Verifique seu e-mail',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-              ),
+      body: StreamBuilder<bool>(
+        stream: Stream.periodic(
+          const Duration(seconds: 1)
+        ).asyncMap((event) => isEmailVerified()),
+        builder: (context, snapshot) {
+          return (snapshot.hasData) ? (snapshot.data == false) ? Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const SizedBox(height: 20,),
+      
+                const Text(
+                  'Verifique seu e-mail',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+      
+                const SizedBox(height: 20,),
+      
+                const Text(
+                  'Enviamos um e-mail para você, verifique sua caixa de entrada e clique no link para confirmar seu e-mail.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+      
+                const SizedBox(height: 20,),
+      
+                const Text(
+                  'Não recebeu o e-mail?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+      
+                const SizedBox(height: 20,),
+      
+                button('Reenviar e-mail', 0, 0, Icons.email, () {
+                  FirebaseAuth.instance.currentUser!.sendEmailVerification().then((value) {
+                    success(context, 'E-mail enviado com sucesso!');
+                  });
+                })
+              ],
             ),
-
-            const SizedBox(height: 20,),
-
-            const Text(
-              'Enviamos um e-mail para você, verifique sua caixa de entrada e clique no link para confirmar seu e-mail.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-              ),
+          ) : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'E-mail verificado com sucesso!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+      
+                const SizedBox(height: 20,),
+      
+                button('Continuar', 0, 0, Icons.arrow_forward_ios, () {
+                  LoginController().redirectUser(context);
+                })
+              ],
             ),
-
-            const SizedBox(height: 20,),
-
-            const Text(
-              'Não recebeu o e-mail?',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-              ),
+          ) : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircularProgressIndicator(),
+      
+                SizedBox(height: 20,),
+      
+                Text(
+                  'Verificando e-mail...',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
             ),
-
-            const SizedBox(height: 20,),
-
-            button('Reenviar e-mail', 0, 0, Icons.email, () {
-              FirebaseAuth.instance.currentUser!.sendEmailVerification().then((value) {
-                success(context, 'E-mail enviado com sucesso!');
-              });
-            })
-          ],
-        ),
+          );
+        }
       ),
     );
   }
