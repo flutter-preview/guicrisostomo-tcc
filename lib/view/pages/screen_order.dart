26c1996fd7303f05b97a383263baa43ab9727a95
guicrisostomo <guilherme.silva9872@gmail.com>
2023-05-21 21:02:09 -0300

    refactor: remove imports/codes not necessary

--- lib/view/pages/screen_order.dart
+++ lib/view/pages/screen_order.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:tcc/controller/postgres/Lists/sales.dart';
