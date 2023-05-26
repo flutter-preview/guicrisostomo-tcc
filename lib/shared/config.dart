ea41cd8019ebd72daeff1da480f25801a4197977
guicrisostomo <guilherme.silva9872@gmail.com>
2023-05-26 15:36:35 -0300

    add splash screen

--- lib/shared/config.dart
+++ lib/shared/config.dart
@@ -12,15 +12,6 @@ class Config {
   static String get apiKeyAndroid => _get('APIKEYANDROID');
   static String get appIdAndroid => _get('APPIDANDROID');
 
-  static String get mysqlHost => _get('MYSQLHOST');
-  static String get mysqlUser => _get('MYSQLUSER');
-  static String get mysqlPassword => _get('MYSQLPASSWORD');
-  static String get mysqlDatabase => _get('MYSQLDB');
-  static String get mysqlPort => _get('MYSQLPORT');
-
-  static String get supabaseUrl => _get('SUPABASEURL');
-  static String get supabaseKey => _get('SUPABASEKEY');
-
   static String get supabaseHost => _get('SUPABASEHOST');
   static int get supabasePort => int.parse(_get('SUPABASEPORT'));
   static String get supabaseUser => _get('SUPABASEUSER');
